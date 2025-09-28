0d37a21fab784d17fd4b7bad4cbd1790fb3262aa
Riquelme Albuquerque <riquelme.albuquerque105@gmail.com>
2025-09-28 15:13:06 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
-//SERVER COMPONENTS
+
 const Home = () => {
-  return <Button>test</Button>
-};
+  return <div>
+    {/* header */}
+    <Header />
+    <div className="p-5">
+      <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+      <p>Domingo, 28 de setembro.</p>
 
-export default Home;
+      <div className="flex items-center gap-2 mt-6">
+      <Input placeholder="Faça sua busca..." />
+      <Button>
+        <SearchIcon></SearchIcon>
+      </Button>
+      </div>
+
+      <div className="relative w-full h-[150px] mt-6">
+        <Image alt="Agende nos melhores com CNC Barber" src="/banner-01.png" fill className="object-cover rounded-xl"/>
+      </div>
+    </div>
+  </div>
+}
+
+export default Home
