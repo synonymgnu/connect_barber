334dfe87731d3fb3988a81dac8f1c715f959f936
synonymgnu <felipe2pac33@gmail.com>
2025-10-09 21:59:39 -0300

    feat: ensure barbershop services are shown on barbershop page

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,6 +3,8 @@ import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from 'next/image';
 import { Button } from '../../_components/ui/button';
 import Link from "next/link"
+import { notFound } from "next/navigation";
+import ServiceItem from "@/app/_components/service-item";
 
 
 
@@ -17,9 +19,19 @@ const BarbershopPage = async ({ params }) => {
     //chamar o banco de dados
     const barbershop = await db.barbershop.findUnique({
         where: {
-            id: params.id
+            id: params.id,
+        },
+        include: {
+
+            services: true,
         }
     })
+
+    if (!barbershop) {
+        return notFound()
+
+    }
+
     return <div>
         {/* IMAGEM */}
         <div className="relative h-[250px] w-full">
@@ -53,20 +65,30 @@ const BarbershopPage = async ({ params }) => {
         </div>
         <div className="p-5 border-b border-solid">
             <h1 className="text-xl font-bold mb-3">{barbershop.name}</h1>
-            <div className=" mb-2 flex items-center gap-1">
+            <div className=" mb-2 flex items-center gap-2">
                 <MapPinIcon className="text-primary" size={18}/>
                 <p>{barbershop.address}</p>
             </div>
 
-            <div className="flex items-center gap-1">
+            <div className="flex items-center gap-2">
                 <StarIcon className="text-primary fill-primary"size={18} />
                 <p className="text-sm"> 5,0 (499 Avaliações)</p>
             </div>
         </div>
 
         {/*DESCRIÇÃO*/}
-        <div className="p-5">
-            <p>{barbershop?.description}</p>
+        <div className="p-5 border-b border-solid space-y-3 ">
+            <h2 className="font-bold uppercase text-gray-400 text-xs">Sobre nós</h2>
+            <p className="text-sn text-justify">{barbershop?.description}</p>
+        </div>
+
+        <div className="p-5 space-y-3">
+        <h2 className="font-bold uppercase text-gray-400 text-xs mb-3">Serviços</h2>
+        <div className="space-y-3">
+        {barbershop.services.map((service => (
+            <ServiceItem key={service.id} service={service}/>
+        )))}
+        </div>
         </div>
     </div>
 }
