06c771baf6fa86d9031bf95abe4dc4f2b32bdb42
Riquelme Albuquerque <riquelme.albuquerque105@gmail.com>
2025-10-21 13:34:02 -0300

    refactor: ensure decimal warning is not shown on console

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -58,7 +58,7 @@ const Bookings = async () => {
                     <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
                     Confirmados
                     </h2>
-                    {confirmedBookings.map(booking => <BookingItem key={booking.id} booking={booking}/> )}
+                    {confirmedBookings.map(booking => <BookingItem key={booking.id} booking={JSON.parse(JSON.stringify(booking))}/> )}
                 </>
             )}
             {concludedBookings.length > 0 && (
@@ -66,7 +66,7 @@ const Bookings = async () => {
                     <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
                     Finalizados
                      </h2>
-                    {concludedBookings.map(booking => <BookingItem key={booking.id} booking={booking}/> )}
+                    {concludedBookings.map(booking => <BookingItem key={booking.id} booking={JSON.parse(JSON.stringify(booking))}/> )}
                 </>
             )}
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -113,7 +113,7 @@ const Home = async () => {
         {/* AGENDAMENTO */}
         <div className="flex overflow-x-auto gap-3 [&::-webkit-scrollbar]:hidden">
           {confirmedBookings.map((booking) => (
-            <BookingItem key={booking.id} booking={booking} />
+            <BookingItem key={booking.id} booking={JSON.parse(JSON.stringify(booking))} />
           ))}
         </div>
         </>
