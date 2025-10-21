1f6d7567d084f74568bc41e29c19142754ed32d5
Riquelme Albuquerque <riquelme.albuquerque105@gmail.com>
2025-10-21 12:59:34 -0300

    feat: ensure booking item takes full width on home page

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,9 @@ const Home = async () => {
           />
         </div>
 
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+        {confirmedBookings.length > 0 && (
+        <>
+          <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Agendamentos
         </h2>
 
@@ -114,6 +116,8 @@ const Home = async () => {
             <BookingItem key={booking.id} booking={booking} />
           ))}
         </div>
+        </>
+        )}
        </div>
 
        {/* LADO DIREITO */}
