7eb867ef215c33d09b77103b77c8ac5abf17e4df
Riquelme Albuquerque <riquelme.albuquerque105@gmail.com>
2025-11-14 12:02:35 -0300

    feat(services): add 50-character limit to service-form description and apply visual truncation in service-item

--- app/_components/dashboard/create-service-dialog.tsx
+++ app/_components/dashboard/create-service-dialog.tsx
@@ -38,7 +38,7 @@ export function CreateServiceDialog() {
           <Button onClick={() => setOpen(true)}>Cadastrar novo</Button>
         </DialogTrigger>
 
-        <DialogContent className="w-[90%] rounded-lg">
+        <DialogContent className="w-[90%] h-[90%]rounded-lg">
           <DialogHeader>
             <DialogTitle>Cadastrar Serviço</DialogTitle>
           </DialogHeader>
