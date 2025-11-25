bb640b2eb0dc14fdbc07c823e31a90270b8b3a9e
YuriXVI <yuri1.xvi@gmail.com>
2025-11-25 16:41:48 -0300

    fix: add duration select in service

--- app/api/reports/top-barbers/route.ts
+++ app/api/reports/top-barbers/route.ts
@@ -45,7 +45,7 @@ export async function GET(request: NextRequest) {
             service: {
               select: {
                 price: true,
-                //duration: true
+                duration: true
               }
             }
           }
